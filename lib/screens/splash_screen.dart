c53e78b840c2af6ad873ea476013c1f41cf0ad37
Gabriel <gariosfe@uide.edu.ec>
2024-11-24 12:28:50 -0500

    ya tiene un buscador

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -72,36 +72,53 @@ class _HomePageState extends State<HomePage> {
         mainAxisAlignment: MainAxisAlignment.start, // Cambiar el alineamiento a la parte superior
         children: [
           // Espacio superior para mover el rectángulo más arriba
-          SizedBox(height: 10.0),  // Ajusta esta altura para mover el rectángulo más arriba o más abajo
-          
+          SizedBox(height: 10.0), // Ajusta esta altura para mover el rectángulo más arriba o más abajo
+
           // Rectángulo en la parte superior de color café
           Container(
-            height: 50.0,  // Altura del rectángulo café
+            height: 50.0, // Altura del rectángulo café
             color: const Color.fromARGB(255, 208, 184, 118), // Color de la barra superior (café)
             width: double.infinity, // Hace que ocupe todo el ancho
             child: Center(
               // Rectángulo blanco dentro del rectángulo café
               child: Container(
-                height: 20.0, // Altura del rectángulo blanco
-                width: 250.0, // Ancho del rectángulo blanco
+                height: 25.0, // Altura del rectángulo blanco
+                width: 300.0, // Ancho del rectángulo blanco
                 color: Colors.white, // Color blanco
+                child: TextField(
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    hintText: 'Search', // Texto de sugerencia
+                    hintStyle: TextStyle(
+                      color: const Color.fromARGB(75, 0, 0, 0), // Cambia el color aquí
+                      fontSize: 14, // Puedes ajustar el tamaño de la fuente
+                    ),
+                    prefixIcon: Icon(
+                      Icons.search, // Icono al lado izquierdo
+                      color: Colors.grey, // Color del ícono
+                    ),
+                  ),
+                ),
               ),
             ),
           ),
-          
+
           Expanded(
             child: Center(
-              child: Text(" "),  // Contenido vacío
+              child: Text(" "), // Contenido vacío
             ),
           ),
-          
+
           // Barra cuadrada en la parte inferior
           Container(
-            height: 60.0,  // Altura fija para la barra inferior
-            color: const Color.fromARGB(255, 208, 184, 118),  // Color de la barra
-            width: double.infinity,  // Hace que ocupe todo el ancho
+            height: 60.0, // Altura fija para la barra inferior
+            color: const Color.fromARGB(255, 208, 184, 118), // Color de la barra
+            width: double.infinity, // Hace que ocupe todo el ancho
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,  // Distribuye los íconos uniformemente
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Distribuye los íconos uniformemente
               children: [
                 IconButton(
                   icon: Icon(Icons.menu),
@@ -118,7 +135,7 @@ class _HomePageState extends State<HomePage> {
                 IconButton(
                   icon: Icon(
                     Icons.photo_camera_outlined,
-                    size: 36.0,  // Aumenta el tamaño del ícono
+                    size: 36.0, // Aumenta el tamaño del ícono
                   ),
                   onPressed: () {
                     // Acción para el icono de cámara
